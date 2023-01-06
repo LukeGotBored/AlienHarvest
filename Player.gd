8e1930cc09d9c6a841ef06ac19d170d1392a077b
LukeGotBored <20886839+LukeGotBored@users.noreply.github.com>
2023-01-06 22:46:17 +0100

    Commit Iniziale

---
+++ Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export (int) var playerSpeed = 200
+var velocity = Vector2()
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * playerSpeed
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
